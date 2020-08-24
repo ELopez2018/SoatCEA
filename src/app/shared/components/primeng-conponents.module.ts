e7d9934759cdeacd638515ce02e47ab558cac204
Estarlin Lopez <45240343+ELopez2018@users.noreply.github.com>
2020-08-24 10:35:09 -0500

    Themes listo

--- src/app/shared/components/primeng-conponents.module.ts
+++ src/app/shared/components/primeng-conponents.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 import { InputTextModule } from 'primeng/inputtext';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
-
+import { MenubarModule } from 'primeng/menubar';
+import { MenuItem } from 'primeng/api';
 
 @NgModule({
   declarations: [],
-  imports: [InputTextModule,  CardModule, ButtonModule],
-  exports: [InputTextModule,  CardModule, ButtonModule],
+  imports: [
+    InputTextModule,
+    CardModule,
+    ButtonModule,
+    MenubarModule],
+  exports: [
+    InputTextModule,
+    CardModule,
+    ButtonModule,
+    MenubarModule
+  ],
 })
 export class PrimengConponentsModule { }
