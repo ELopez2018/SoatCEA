aceaf2075a0ccb301602922ba8dd1c1e37b1327a
Estarlin Lopez <45240343+ELopez2018@users.noreply.github.com>
2020-08-24 09:48:17 -0500

    Incorporacion de FlexBox

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { appConstants } from '../../global/constants/index';
+
 
 @Component({
   selector: 'app-login',
@@ -6,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  btnSave = appConstants.BUTTON_SAVE;
+  btnDelete = appConstants.BUTTON_DELETE;
+  btnCancel = appConstants.BUTTON_CANCEL;
   constructor() { }
 
   ngOnInit(): void {
--- src/app/shared/components/primeng-conponents.module.ts
+++ src/app/shared/components/primeng-conponents.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-
-  ]
+  imports: [InputTextModule,  CardModule, ButtonModule],
+  exports: [InputTextModule,  CardModule, ButtonModule],
 })
 export class PrimengConponentsModule { }
