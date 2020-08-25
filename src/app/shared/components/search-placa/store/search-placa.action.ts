9ff89d0edbd97e1d6a25fdc0745596c239c1cafd
Estarlin Lopez <45240343+ELopez2018@users.noreply.github.com>
2020-08-25 13:36:20 -0500

    Creacion  Vista HTML

--- src/app/shared/components/search-placa/store/search-placa.action.ts
+++ src/app/shared/components/search-placa/store/search-placa.action.ts
@@ -1,5 +1,26 @@
 import { createAction, props } from '@ngrx/store';
+import { ApiActionModel } from '../../../models/api-action.model';
+import { ResponsePlacaModel } from '../../../models/response-placa.model';
 
-ResponsePlacaModel
 
-ApiActionModel
+
+export const currentSearchPlaca = createAction(
+  '[SearchPlaca] Current Search-Placa',
+  props<{ responsePlacaModel: ResponsePlacaModel }>()
+);
+
+export const selectSearchPlaca = createAction(
+  '[SearchPlaca] Select Search-Placa',
+  props<{ responsePlacaModel: ResponsePlacaModel }>()
+);
+
+export const searchSearchPlaca = createAction(
+  '[SearchPlaca] Search Search-Placa',
+  props<{ action: ApiActionModel<ResponsePlacaModel> }>()
+);
+
+export const searchSearchPlacaSuccess = createAction(
+  '[SearchPlaca] Search Search-Placa Success',
+  props<{ responsePlacaModel: ResponsePlacaModel }>()
+);
+
